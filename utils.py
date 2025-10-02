2d846734804e94b458e50752cbda51d65c81c3b7
Wolyo <144167357+Kaio-Emidio@users.noreply.github.com>
2025-10-01 23:58:59 -0300

    MUITOS AJUSTES e organização das pastas :D

---
+++ utils.py
@@ -0,0 +1,25 @@
+from mysql.connector import (connection)
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def ConectarBD():
+    cnx = connection.MySQLConnection(
+        user=os.getenv('USERDB'), 
+        password=os.getenv('PASSDB'), 
+        host='127.0.0.1', 
+        database=os.getenv('NAMEDB')
+        )
+    
+    return cnx
+
+def InserirAlterarRemover(sql, dados):
+    cnx = ConectarBD()
+
+    cursor = cnx.cursor()
+
+    cursor.execute(sql, dados)
+    cnx.commit()
+
+    cnx.close()
