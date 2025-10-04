760afae06a9cec2db49f471142014c8d2d18e893
vinicius-gab <144168249+vinicius-gab@users.noreply.github.com>
2025-10-03 22:39:53 -0300

    Adicionada a opção de cadastrar conteudos

--- app.py
+++ app.py
@@ -43,6 +43,25 @@ def conf_cad_user():
 
     return render_template('login_usuario.html')
 
+@app.route('/cadastrar_conteudo', methods=['GET', 'POST'])
+def conf_cad_cont():
+    if request.method == 'POST':
+        nome = request.form.get('Nome_do_conteudo')
+        sinopse = request.form.get('descricao')
+        url = request.form.get('Caminho_para_conteudo')
+        capa = request.form.get('Link_para_capa') or None
+        autor = request.form.get('Autor')
+        data = request.form.get('Data_de_publicacao')
+        categoria = request.form.get('tipo_conteudo')
+
+        sql = 'INSERT INTO conteudo (Nome, sinopse, url, capa, autor, data, categoria) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+        dados = (nome, sinopse, url, capa, autor, data, categoria)
+        InserirAlterarRemover(sql, dados)
+
+        return render_template('cadastro_conteudo.html', sucesso=True)
+
+    return render_template('cadastro_conteudo.html')
+
 @app.route('/favoritos')
 def favoritos():
     return render_template('favoritos.html')
